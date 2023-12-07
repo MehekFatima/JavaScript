17b3502de53d4ef44907a161a7c46eb8a9064abe
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-12-07 21:16:33 +0530

    forEach.js

--- forEach.js
+++ forEach.js
@@ -4,3 +4,24 @@ let numbers = [1, 2, 3, 4, 5];
 numbers.forEach(function(element){
     console.log(element);
 });
+
+// map method to print squares of an array
+
+let numbers = [1, 2, 3, 4, 5];
+function squares(element){
+    return Math.pow(element,2);
+}
+let square = numbers.map(squares);
+square.forEach(function(element){
+    console.log(element);
+});
+
+// same code using forEach method
+let numbers = [1, 2, 3, 4, 5];
+function squares(element,index,array){
+    array[index] = Math.pow(element,2);
+}
+numbers.forEach(squares);
+numbers.forEach(function(element){
+    console.log(element);
+});
